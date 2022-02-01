b22cefd35cb53d5671ad10231729d4dc38e0bcee
Roberto Andrade <srandradejr@gmail.com>
2022-02-01 19:09:21 +0000

    basic usage

--- src/my-element.ts
+++ src/my-element.ts
@@ -38,8 +38,21 @@ export class MyElement extends LitElement {
   count = 0;
 
   override render() {
+    const canvas = document.createElement('canvas')
+    canvas.innerText = 'Fallback Content'
+    const ctx = canvas.getContext('2d')
+    canvas.width = 800
+    canvas.height = 800;
+    if (ctx) {
+      ctx.fillStyle = 'rgb(200, 0, 0)';
+      ctx.fillRect(10, 10, 50, 50);
+
+      ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+      ctx.fillRect(30, 30, 50, 50);
+    }
     return html`
       <h1>${this.sayHello(this.name)}!</h1>
+      ${canvas}
       <button @click=${this._onClick} part="button">
         Click Count: ${this.count}
       </button>
